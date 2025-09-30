e2c3a6d2fbf23bb2da5c80170ac46c3c1d57404d
Tivis7 <106712424+Kevin-REU@users.noreply.github.com>
2025-09-30 16:59:23 -0700

    Added Hello World to program.
    The Repo should be nicer now!

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+#	Kevin Gustavo Roldan
+#	ASTR-19
+#	2025/09/30
+
+
+# This program will write: "Buenos Días Compadre!"
+
+
+# Funtion Tells Comp. to print: "Buenos Días Compadre!"
+def PrintBDC():
+	print("\n\n Buenos Días Compadre! \n\n")
+
+# This defines our main() function for this program
+def main():
+	PrintBDC()
+
+
+# When we run this program this executes first
+if __name__=="__main__":
+	main()
